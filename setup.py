f20892ec7d469513a7c66e7bae7039a1db955d3c
Yunus Emre Çelikkıran <>
2023-09-01 15:14:48 +0300

    color library changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='logger_lib',
-    version='0.2.0',
+    version='0.3.0',
     description='Logger Library for Gauge Test',
     packages=find_packages(),
     install_requires=['wheel', 'colorlog'],
