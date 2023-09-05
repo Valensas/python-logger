216333285c638d4f64ddc32a27eaee460f9e1c50
Yunus Emre Çelikkıran <>
2023-09-05 11:02:08 +0300

    add rich lib

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.3.0',
     description='Logger Library for Gauge Test',
     packages=find_packages(),
-    install_requires=['wheel', 'colorlog'],
+    install_requires=['wheel', 'rich'],
 )
